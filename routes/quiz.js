e1c0ab763181593ef02e7558cb9ba4e6ae7f6993
Sebastian Skora <sebastian.skora27@gmail.com>
2020-04-03 15:38:02 +0200

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var router = express.Router();
-
+const Quiz = require("../models/quiz");
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("quiz", { title: "Quiz" });
+  const show = !req.session.vote;
+  Quiz.find({}, (err, data) => {
+    let sum = 0;
+    data.forEach(item => {
+      sum += item.vote;
+    });
+    res.render("quiz", { title: "Quiz", data, show, sum });
+  });
+});
+
+router.post("/", function(req, res, next) {
+  const id = req.body.quiz;
+  Quiz.findOne({ _id: id }, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save(err => {
+      req.session.vote = 1;
+
+      res.redirect("/quiz");
+    });
+  });
 });
 
 module.exports = router;
